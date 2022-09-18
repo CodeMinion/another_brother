f2f203b584e6b05c786a3f033f7478265435fe0d
Frank Hernandez <hernandez.f@rouninlabs.com>
2022-09-17 23:11:48 -0400

    Resolved issue causing the app to freeze when calling printImage on iOS

--- ios/Classes/Method/PrintImageMethodCall.m
+++ ios/Classes/Method/PrintImageMethodCall.m
@@ -26,50 +26,68 @@ static NSString * METHOD_NAME = @"printImage";
     return METHOD_NAME;
 }
 - (void)execute {
-    // Get printInfo dart params from call
-    NSDictionary<NSString *, NSObject *> * dartPrintInfo = _call.arguments[@"printInfo"];
-    // Get image bytes from call
-    FlutterStandardTypedData * imageData = _call.arguments[@"imageBytes"];
-    NSData * imageBytes = imageData.data;
-    
-    //NSData * imageBytes = [FlutterStandardTypedData typedDataWithBytes:_call.arguments[@"imageBytes"]].data;
-    
-    // TODO Get channel from printInfo
-    BRLMChannel *channel = [BrotherUtils printChannelWithPrintSettingsMap:dartPrintInfo];
-    
-    // TODO Generate printer driver
-    BRLMPrinterDriverGenerateResult * driverGenerateResult = [BRLMPrinterDriverGenerator openChannel:channel];
-        if (driverGenerateResult.error.code != BRLMOpenChannelErrorCodeNoError ||
-            driverGenerateResult.driver == nil) {
-            
-            // On Error report error
-            NSDictionary<NSString *, NSObject *> * printStatus = [BrotherUtils printerStatusToMapWithError:BRLMPrintErrorCodePrinterStatusErrorCommunicationError  status:nil];
-            _result(printStatus);
-            return;
-        }
 
-    
-    BRLMPrinterDriver *printerDriver = driverGenerateResult.driver;
-    
-    // Get printer settings.
-    id<BRLMPrintSettingsProtocol>  printerSettings = [BrotherUtils printSettingsFromMapWithValue:dartPrintInfo];
-    
-    CGDataProviderRef imgDataProvider = CGDataProviderCreateWithCFData((__bridge CFDataRef)(imageBytes));
-    CGImageRef imageRef = CGImageCreateWithPNGDataProvider
-(imgDataProvider, NULL, true, kCGRenderingIntentDefault);
-    
-    // Call print method
-    BRLMPrintError * printError = [printerDriver printImageWithImage:imageRef settings:printerSettings];
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0ul);
+                dispatch_async(queue, ^{
 
-    
-    [printerDriver closeChannel];
-    
-    // Notify status to Flutter.
-    NSDictionary<NSString *, NSObject *> * printStatus = [BrotherUtils printerStatusToMapWithError:printError.code  status:nil];
-    
-    _result(printStatus);
-    
-   
+
+        // Get printInfo dart params from call
+        NSDictionary<NSString *, NSObject *> * dartPrintInfo = _call.arguments[@"printInfo"];
+        // Get image bytes from call
+        FlutterStandardTypedData * imageData = _call.arguments[@"imageBytes"];
+        NSData * imageBytes = imageData.data;
+
+        //NSData * imageBytes = [FlutterStandardTypedData typedDataWithBytes:_call.arguments[@"imageBytes"]].data;
+
+        // TODO Get channel from printInfo
+        BRLMChannel *channel = [BrotherUtils printChannelWithPrintSettingsMap:dartPrintInfo];
+
+        // TODO Generate printer driver
+        BRLMPrinterDriverGenerateResult * driverGenerateResult = [BRLMPrinterDriverGenerator openChannel:channel];
+            if (driverGenerateResult.error.code != BRLMOpenChannelErrorCodeNoError ||
+                driverGenerateResult.driver == nil) {
+
+                // On Error report error
+                NSDictionary<NSString *, NSObject *> * printStatus = [BrotherUtils printerStatusToMapWithError:BRLMPrintErrorCodePrinterStatusErrorCommunicationError  status:nil];
+                //_result(printStatus);
+
+                dispatch_sync(dispatch_get_main_queue(), ^{
+                                            // Update UI
+                                            self->_result(printStatus);
+                                        });
+
+                return;
+            }
+
+
+        BRLMPrinterDriver *printerDriver = driverGenerateResult.driver;
+
+        // Get printer settings.
+        id<BRLMPrintSettingsProtocol>  printerSettings = [BrotherUtils printSettingsFromMapWithValue:dartPrintInfo];
+
+        CGDataProviderRef imgDataProvider = CGDataProviderCreateWithCFData((__bridge CFDataRef)(imageBytes));
+        CGImageRef imageRef = CGImageCreateWithPNGDataProvider
+    (imgDataProvider, NULL, true, kCGRenderingIntentDefault);
+
+        // Call print method
+        BRLMPrintError * printError = [printerDriver printImageWithImage:imageRef settings:printerSettings];
+
+
+        [printerDriver closeChannel];
+
+        // Notify status to Flutter.
+        NSDictionary<NSString *, NSObject *> * printStatus = [BrotherUtils printerStatusToMapWithError:printError.code  status:nil];
+
+        //_result(printStatus);
+
+                            // Notify on the UI thread.
+                            dispatch_sync(dispatch_get_main_queue(), ^{
+                                    // Update UI
+                                    // Call the desired channel message here.
+                                    self->_result(printStatus);
+                                });
+
+       });
 }
 
 
