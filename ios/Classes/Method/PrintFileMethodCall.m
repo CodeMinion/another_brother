38779d76bd3283921d34413ba3ed74ca60104803
Artur Dziombek <arturdziombek@Arturs-MacBook-Pro.local>
2024-11-17 00:38:31 +0100

    printing on background thread

--- ios/Classes/Method/PrintFileMethodCall.m
+++ ios/Classes/Method/PrintFileMethodCall.m
@@ -26,62 +26,62 @@ static NSString * METHOD_NAME = @"printFile";
     return METHOD_NAME;
 }
 - (void)execute {
-    // TODO Move to background thread.
+    // Move the execution to a background thread to avoid blocking the main thread
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0ul);
+    dispatch_async(queue, ^{
 
-    // Get printInfo dart params from call
-    NSDictionary<NSString *, NSObject *> * dartPrintInfo = _call.arguments[@"printInfo"];
-    // Get file path from call
-    NSString * filePath = _call.arguments[@"filePath"];
-    
-    
-    // TODO Get channel from printInfo
-    BRLMChannel *channel = [BrotherUtils printChannelWithPrintSettingsMap:dartPrintInfo];
-    
-    // TODO Generate printer driver
-    BRLMPrinterDriverGenerateResult * driverGenerateResult = [BRLMPrinterDriverGenerator openChannel:channel];
+        // Get printInfo dart params from call
+        NSDictionary<NSString *, NSObject *> * dartPrintInfo = _call.arguments[@"printInfo"];
+        // Get file path from call
+        NSString * filePath = _call.arguments[@"filePath"];
+
+        // Get channel from printInfo
+        BRLMChannel *channel = [BrotherUtils printChannelWithPrintSettingsMap:dartPrintInfo];
+
+        // Generate printer driver
+        BRLMPrinterDriverGenerateResult * driverGenerateResult = [BRLMPrinterDriverGenerator openChannel:channel];
         if (driverGenerateResult.error.code != BRLMOpenChannelErrorCodeNoError ||
             driverGenerateResult.driver == nil) {
-            
+
             // On Error report error
-            NSDictionary<NSString *, NSObject *> * printStatus = [BrotherUtils printerStatusToMapWithError:BRLMPrintErrorCodePrinterStatusErrorCommunicationError  status:nil];
-            _result(printStatus);
+            NSDictionary<NSString *, NSObject *> * printStatus = [BrotherUtils printerStatusToMapWithError:BRLMPrintErrorCodePrinterStatusErrorCommunicationError status:nil];
+
+            // Notify Flutter on the main thread
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                self->_result(printStatus);
+            });
             return;
         }
 
-    
-    BRLMPrinterDriver *printerDriver = driverGenerateResult.driver;
-    
-    // Get printer settings.
-    id<BRLMPrintSettingsProtocol>  printerSettings = [BrotherUtils printSettingsFromMapWithValue:dartPrintInfo];
-    
-    // If no error create URL from path
-    
-    NSURL * url = [NSURL fileURLWithPath:filePath];
-    
-    NSString * extension = [url pathExtension];
-    BRLMPrintError * printError;
-    
-    if ([extension isEqualToString:@"prn"] ) {
-        // TODO Print PRN
-        // Call print method
-        printError = [printerDriver sendPRNFileWithURL:url];
-    }
-    else {
-        // Print normal file
-        // Call print method
-        printError = [printerDriver printImageWithURL:url settings:printerSettings];
-    }
-    
-    
-    
-    [printerDriver closeChannel];
-    
-    // Notify status to Flutter.
-    NSDictionary<NSString *, NSObject *> * printStatus = [BrotherUtils printerStatusToMapWithError:printError.code  status:nil];
-    
-    _result(printStatus);
-    
-   
+        BRLMPrinterDriver *printerDriver = driverGenerateResult.driver;
+
+        // Get printer settings.
+        id<BRLMPrintSettingsProtocol> printerSettings = [BrotherUtils printSettingsFromMapWithValue:dartPrintInfo];
+
+        // If no error, create URL from file path
+        NSURL *url = [NSURL fileURLWithPath:filePath];
+        NSString *extension = [url pathExtension];
+        BRLMPrintError *printError;
+
+        if ([extension isEqualToString:@"prn"]) {
+            // Print PRN file
+            printError = [printerDriver sendPRNFileWithURL:url];
+        } else {
+            // Print normal file (image)
+            printError = [printerDriver printImageWithURL:url settings:printerSettings];
+        }
+
+        // Close printer channel
+        [printerDriver closeChannel];
+
+        // Notify status to Flutter
+        NSDictionary<NSString *, NSObject *> *printStatus = [BrotherUtils printerStatusToMapWithError:printError.code status:nil];
+
+        // Update Flutter UI on the main thread
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            self->_result(printStatus);
+        });
+    });
 }
 
 
